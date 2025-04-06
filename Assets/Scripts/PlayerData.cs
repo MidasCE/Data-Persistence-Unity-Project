7b77ab2d0a51d53645d335ab0fd98deb9a071e77
pattarapol_tiensermpong <midaskmitl@gmail.com>
2025-04-06 15:04:57 +0700

    Add textmeshpro, prepare PlayerData class as singleton to handle score data

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public static PlayerData Instance;
+    
+    
+    public int HighestScore { get; private set; }
+    public string HighestScorePlayerName { get; private set; }
+    
+    public string PlayerName { get; set; }
+    public int PlayerScore { get; set; }
+    
+    public void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void OnGameOver(int gameOverScore)
+    {
+        PlayerScore = gameOverScore;
+        if (PlayerScore > HighestScore)
+        {
+            SaveNewPlayerScore();
+        }
+    }
+
+    private void SaveNewPlayerScore()
+    {
+        HighestScore = PlayerScore;
+        HighestScorePlayerName = PlayerName;
+            
+        SaveData data = new SaveData();
+        data.HighestScore = PlayerScore;
+        data.HighestScorePlayerName = PlayerName;
+
+        string json = JsonUtility.ToJson(data);
+  
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+    
+    public void LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        Debug.Log(path);
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            HighestScore = data.HighestScore;
+            HighestScorePlayerName = data.HighestScorePlayerName;
+        }
+        else
+        {
+            HighestScore = 0;
+            HighestScorePlayerName = "NaN";
+        }
+
+        PlayerScore = 0;
+        PlayerName = "NaN";
+    }
+}
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,6 @@
+﻿[System.Serializable]
+public class SaveData
+{
+    public int HighestScore;
+    public string HighestScorePlayerName;
+}
